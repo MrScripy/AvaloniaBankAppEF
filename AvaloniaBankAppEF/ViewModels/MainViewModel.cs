9abf1070b803b8707707606b7c620948f2e03aca
Rina-Scr <muntyanu.kairna@yandex.ru>
2024-01-10 18:11:30 +0300

    added DataGrid with styles

--- AvaloniaBankAppEF/ViewModels/MainViewModel.cs
+++ AvaloniaBankAppEF/ViewModels/MainViewModel.cs
@@ -1,9 +1,422 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
 namespace AvaloniaBankAppEF.ViewModels
 {
-    
-
-    internal class MainViewModel:ViewModelBase
+    internal class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+    }
+
+    internal class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+
+    internal partial class MainViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private ObservableCollection<Customer> _customers = new ObservableCollection<Customer>
+        {
+            new Customer 
+            { 
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },
+             new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },
+              new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            }, new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },new Customer
+            {
+                Id = 1,
+                Name = "Foo",
+                Patronymic = "Bar",
+                Surname = "Bar",
+                Phone = "Bar",
+                Mail = "Bar"
+            },
+        };
+
+        [ObservableProperty]
+        private ObservableCollection<Order> _orders = new ObservableCollection<Order>
+        {
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+            new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            }, new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },new Order
+            {
+                Id= 1,
+                Name = "Foo",
+                Code = "Bar"
+            },
+
+        };
+
+        [ObservableProperty]
+        private string _test = "Test";
+
     }
 }
